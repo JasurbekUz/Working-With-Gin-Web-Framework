edce909260a4d1867189a6839781eb8050538c08
JasurbekUz <jasurbekedu@gmail.com>
2021-10-23 18:26:14 +0500

    my apps

--- 1_gin-gonic/signUp_logIn/_set_up/utils.sql
+++ 1_gin-gonic/signUp_logIn/_set_up/utils.sql
@@ -1,1 +1,35 @@
-utils.sql
+create or replace function users_trigger_func() returns trigger language plpgsql as $$
+
+  declare
+
+     u_name varchar;
+     u_email varchar;
+
+  begin
+
+    select u.user_name from users as u into u_name where u.user_name = new.user_name;
+    
+      if found then
+
+          return null;
+
+      end if;          
+
+    select u.email from users as u into u_email where u.email= new.email;
+     
+      if found then
+
+          return null;
+          
+      end if;
+
+    return new;
+    
+  end;
+
+$$;
+
+
+create trigger user_trigger
+before insert on users
+for each row execute procedure users_trigger_func();
